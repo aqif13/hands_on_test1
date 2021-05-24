84d43d814ee87baaec1cf6ffc7c87f3cee7205d8
aqif13 <79740494+aqif13@users.noreply.github.com>
2021-05-24 20:21:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-print "Hello Guys, I am Wan Muhamad Syaqif Bin Wan Safri, the Cloud Computer expert!."\n"
-Please go to my GitHub account https://github.com/aqif13";
+print "Hello Guys, I am Wan Muhamad Syaqif Bin Wan Safri, the Cloud Computer expert!.Please go to my GitHub account https://github.com/aqif13";
 ?>
 
 >. 
